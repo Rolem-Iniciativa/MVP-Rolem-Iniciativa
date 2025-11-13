e7ef2975bb5453c1aa591839d8927453e5fd8bdf
Gle1sonFreitas <gleison.freitas@sptech.school>
2025-11-12 23:06:59 -0300

    parametrização do card login com a api

---
+++ web-data-viz/public/js/login.js
@@ -0,0 +1,70 @@
+function CardLogin(Abrir){
+
+    if (Abrir == false){
+        return Abrir =
+        card_login.style = 'display: flex;', 
+        user_icon.onclick = () => CardLogin(true),
+        console.log("abriu");
+    } 
+    
+    if(Abrir == true){
+        return Abrir = 
+        card_login.style = 'display: none;',
+        user_icon.onclick = () => CardLogin(false),
+        console.log('fechou');
+    }
+    
+}
+
+function Entrar() {
+
+    var emailVar = ipt_email.value;
+    var senhaVar = ipt_senha.value;
+
+    console.log("FORM LOGIN: ", emailVar);
+    console.log("FORM SENHA: ", senhaVar);
+
+    fetch("/usuarios/autenticar", {
+        method: "POST",
+        headers: {
+    "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+    emailServer: emailVar,
+    senhaServer: senhaVar
+        })
+    }).then(function (resposta) {
+        console.log("ESTOU NO THEN DO entrar()!")
+
+        if (resposta.ok) {
+    console.log(resposta);
+
+    resposta.json().then(json => {
+        console.log(json);
+        console.log(JSON.stringify(json));
+        sessionStorage.ID_USUARIO = json.idUsuario;
+        sessionStorage.NOME_USUARIO = json.nome;
+        sessionStorage.NICKNAME_USUARIO = json.nickName;
+        sessionStorage.EMAIL_USUARIO = json.email;
+
+        setTimeout(function () {
+    window.location = "./dashboard/cards.html";
+        }, 1000); // apenas para exibir o loading
+
+    });
+
+        } else {
+
+    console.log("Houve um erro ao tentar realizar o login!");
+
+    resposta.text().then(texto => {
+        console.error(texto);
+    });
+        }
+
+    }).catch(function (erro) {
+        console.log(erro);
+    })
+
+    return false;
+}
---
+++ web-data-viz/src/database/TabelasRolemIniciativa.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE BD_RolemIniciativa;
+USE BD_RolemIniciativa;
+
+CREATE TABLE Usuario (
+idUsuario int primary key auto_increment,
+nome varchar(100),
+nickName varchar (100),
+email varchar (100),
+senha varchar (100),
+xp int
+
+);
